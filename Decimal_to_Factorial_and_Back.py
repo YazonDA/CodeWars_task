c85ec5c0fcf95b31c47291e4e04b0cfb91437406
YazonDA <yazonda.29@gmail.com>
2019-09-16 22:39:46 +0300

    more one CodeWars task - Decimal to Factorial and Back - init

---
+++ Decimal_to_Factorial_and_Back.py
@@ -0,0 +1,18 @@
+'''CodeWars task - Decimal to Factorial and Back
+Tag`s - ALGORITHMS NUMBERS UTILITIES
+5 kyu
+We will code two functions. The first one will code a decimal number and return a string with the factorial representation : dec2FactString(nb)
+The second one will decode a string with a factorial representation and produce the decimal representation : factString2Dec(str).
+Given numbers will be positive.'''
+
+
+def dec2FactString(nb):
+  #your code here
+
+
+def factString2Dec(string):
+  #your code here
+
+
+'''test.assert_equals(dec2FactString(463), "341010")
+test.assert_equals(factString2Dec("341010"), 463)'''
