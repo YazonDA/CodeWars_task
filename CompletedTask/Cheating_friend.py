646d7cc3477e5a41dc71c88f229ad3beee2fb1d0
YazonDA <yazonda.29@gmail.com>
2019-10-27 19:47:24 +0300

    more one task - completed

--- CompletedTask/Cheating_friend.py
+++ CompletedTask/Cheating_friend.py
@@ -10,24 +10,25 @@ Array will be sorted in increasing order of the "a".'''
 
 import pysnooper
 
-@pysnooper.snoop()
+#@pysnooper.snoop()
 def removNb(n):
 	seqw = list(range(1, n + 1))
 	sum_seqw = sum(seqw)
 	ans = []
-	for JwJ in seqw[:n//2+1]:
-		for i in seqw[seqw.index(JwJ) + 1:]:
+	for JwJ in seqw:
+		for i in seqw[(sum_seqw // JwJ) - 3:(sum_seqw // JwJ) - 1]:
 			w_sum = sum_seqw - JwJ - i
-			if w_sum/JwJ == i:
+			if (sum_seqw - JwJ - i)/JwJ == i:
 				ans.append((JwJ, i))
 				ans.append((i, JwJ))
-				seqw.pop(seqw.index(JwJ))		
-	return ans
+				seqw.pop(seqw.index(JwJ))
+				break
+	return sorted(ans)
 
 
 ask = [
-[26, [(15, 21), (21, 15)]]#,
-#[100, []]
+[26, [(15, 21), (21, 15)]],
+[1000, []]
 ]
 
 for i in range(len(ask)):
@@ -35,3 +36,21 @@ for i in range(len(ask)):
 	ans_txt = 'Correct' if answer == ask[i][1] else 'Wrong'
 	print(f'Answer {answer} is {ans_txt}!')
 
+''' I need more mathematiks :((((
+IT`S CORRECT SOLUTION. NOT MY!
+def removNb(n):
+    total = n*(n+1)/2
+    sol = []
+    for a in range(1,n+1):
+        b = (total-a)/(a+1.0)
+        if b.is_integer() and b <= n:
+            sol.append((a,int(b)))
+    return sol
+
+def removNb(n):
+    sum = n*(n + 1)/2  
+    return [
+    (x, (sum - x) / (x + 1))
+    	for x in xrange(1, n+1)
+    		if (sum - x) % (x + 1) == 0 and 1 <= (sum - x) / (x + 1) <= n
+    		]'''
