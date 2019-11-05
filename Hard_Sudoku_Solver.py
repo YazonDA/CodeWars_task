df202a43e5fd8ed2870d12d38a7357ba140891ba
yazon <yazonda.29@gmail.com>
2019-11-05 09:44:40 +0300

    go to the pc

--- Hard_Sudoku_Solver.py
+++ Hard_Sudoku_Solver.py
@@ -200,11 +200,14 @@ def sudoku_solver(puzzle):
 		return
 
 	w_arr = create_w_arr()
-	f_ans = count_ans()
+	#f_ans = count_ans()
 
 	while f_ans < 81:
 		print(count_ans())
-		clean_extra_suppose()
+		#f_ans = count_ans()
+		while f_ans != count_ans():
+			f_ans = count_ans()
+			clean_extra_suppose()
 		print_arr(w_arr)
 	
 		if unique_look_clean(w_arr):
@@ -215,7 +218,6 @@ def sudoku_solver(puzzle):
 						
 								while double_mixed(w_arr):
 									remove_unique_double_mixed(w_arr)'''
-		f_ans +=1
 		input()
 
 
@@ -223,15 +225,15 @@ def sudoku_solver(puzzle):
 
 
 ask = [[
-		[0, 0, 6, 1, 0, 0, 0, 0, 8], 
-		[0, 8, 0, 0, 9, 0, 0, 3, 0], 
-		[2, 0, 0, 0, 0, 5, 4, 0, 0], 
-		[4, 0, 0, 0, 0, 1, 8, 0, 0], 
-		[0, 3, 0, 0, 7, 0, 0, 4, 0], 
-		[0, 0, 7, 9, 0, 0, 0, 0, 3], 
-		[0, 0, 8, 4, 0, 0, 0, 0, 6], 
-		[0, 2, 0, 0, 5, 0, 0, 8, 0], 
-		[1, 0, 0, 0, 0, 2, 5, 0, 0]
+		[6, 8, 0, 0, 3, 0, 0, 0, 4], 
+		[0, 3, 4, 0, 0, 0, 2, 0, 0], 
+		[0, 0, 0, 7, 0, 0, 0, 0, 5], 
+		[5, 0, 8, 4, 0, 0, 0, 1, 0], 
+		[0, 4, 2, 9, 0, 0, 0, 0, 0], 
+		[1, 0, 0, 0, 0, 0, 0, 5, 0], 
+		[0, 2, 0, 8, 0, 1, 0, 4, 0], 
+		[0, 0, 0, 0, 0, 9, 6, 0, 0], 
+		[0, 0, 0, 0, 0, 0, 0, 0, 8]
 		], [
 		[3, 4, 6, 1, 2, 7, 9, 5, 8], 
 		[7, 8, 5, 6, 9, 4, 1, 3, 2], 
