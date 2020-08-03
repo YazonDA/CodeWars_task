9b7f88eda8af67fe44ffef05fc607188db488d83
YazonDA <yazonda.29@gmail.com>
2020-08-03 04:32:21 +0000

    some tasks is completed

--- tst.py
+++ tst.py
@@ -1,16 +1,15 @@
-def consecutive_ducks(n):
-	di = n // 2
-	ui = di + 1
-	check_sum = di + ui
-	while True:
-		if check_sum <= 1: return False
-		elif check_sum > n:
-			check_sum -= ui
-			ui -= 1
-		elif check_sum == n: return True
-		di -= 1
-		check_sum += di
+def make_next_string(old_string):
+	new_string = [1]
+	for k in range(1, len(old_string)):
+		new_string.append(old_string[k - 1] + old_string[k])
+	new_string.append(1)
+	return new_string
 
 
-for q in range(1, 150):
-	print(f'for {q} --> {consecutive_ducks(q)}')
+quest = 3
+old_str = [1]
+for n in range(quest):
+	print(old_str)
+	old_str = make_next_string(old_str)
+answer = [i ** 2 for i in old_str]
+print(f'\n{sum(answer)}')
