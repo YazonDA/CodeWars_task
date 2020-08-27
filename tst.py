445e4559b33cd7a5d39ff26d60940c6a0c3161ff
YazonDA <yazonda.29@gmail.com>
2020-08-27 15:08:24 +0000

    it`s not important, but changed tst.py

--- tst.py
+++ tst.py
@@ -1,15 +1,55 @@
-def make_next_string(old_string):
-	new_string = [1]
-	for k in range(1, len(old_string)):
-		new_string.append(old_string[k - 1] + old_string[k])
-	new_string.append(1)
-	return new_string
+def isBadVersion(version):
+	return version >= tst_bad_version
 
+def _firstBadVersion(n):
+	if not isBadVersion(n):
+		return (None, 1)
 
-quest = 3
-old_str = [1]
-for n in range(quest):
-	print(old_str)
-	old_str = make_next_string(old_str)
-answer = [i ** 2 for i in old_str]
-print(f'\n{sum(answer)}')
+	if isBadVersion(n):
+		if not isBadVersion(n-1):
+			return n
+		else:
+			return firstBadVersion(n-1) 
+	if not isBadVersion(n):
+		if isBadVersion(n+1):
+			return n+1
+		else:
+			return firstBadVersion(n+2)
+
+def firstBadVersion(n):
+	if not isBadVersion(n):
+		return (None, 1)
+
+	good_version = 0
+	bad_version = n
+	while bad_version - good_version - 1:
+		print(f'{bad_version} - {good_version}')
+		if isBadVersion(bad_version - (bad_version - good_version) // 2):
+			bad_version -= (bad_version - good_version) // 2
+		else:
+			good_version += (bad_version - good_version) // 2
+	return bad_version
+
+tst_bad_version = 8
+#tst_bad_version = 2126753390
+#tst_bad_version = 1702766719
+
+print(tst_bad_version - 1)
+print(int(tst_bad_version * 1.1) + 1)
+for i in range(tst_bad_version - 1, int(tst_bad_version * 1.1) + 1):
+	print(f'actual version is {i}, this version is {not isBadVersion(i)}')
+	print(f'the first Bad Version is {firstBadVersion(i)}\n')
+
+'''
+def check(n):
+	if n is Bad:
+		if n-1 is Good:
+			return n
+		else:
+			return check(n-1) 
+	if n is Good:
+		if n+1 is Bad:
+			return n+1
+		else:
+			return check(n+2)
+'''
